64eb4bc304753a31a7ec931ed20062937530cebc
upendra bommali <upendrabommali811@gmail.com>
2023-11-18 12:00:01 +0530

    hiii

---
+++ src/main/java/org/dbc/aaa/App.java
@@ -0,0 +1,13 @@
+package org.dbc.aaa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
